50fc9ac1518d1c87ce1f0a3ec472aed743129c8a
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2022-05-09 19:18:46 -0400

    Update Install-DotNet6.ps1

--- resources/scripts/Install-DotNet6.ps1
+++ resources/scripts/Install-DotNet6.ps1
@@ -1,6 +1,6 @@
 <#
     - [Name] Install-DotNet6.ps1
-    - [Description] Installs .NET 6.0.4
+    - [Description] Downloads and installs the Windows Desktop Runtime v6.0.4 (x64)
     - [Resources and References]
         -
 #>
@@ -11,13 +11,15 @@ Write-Host
 # Define Variables
 $ProgressPreference = 'SilentlyContinue'
 $ErrorActionPreference = 'Stop'
-$dotnetUrl = "https://download.visualstudio.microsoft.com/download/pr/f13d7b5c-608f-432b-b7ec-8fe84f4030a1/5e06998f9ce23c620b9d6bac2dae6c1d/windowsdesktop-runtime-6.0.4-win-x64.exe"
+$backup_dotnetUrl = "https://download.visualstudio.microsoft.com/download/pr/f13d7b5c-608f-432b-b7ec-8fe84f4030a1/5e06998f9ce23c620b9d6bac2dae6c1d/windowsdesktop-runtime-6.0.4-win-x64.exe"
+$dotnetUrl = "https://datacdn.b-cdn.net/dl/windowsdesktop-runtime-6.0.4-win-x64.exe"
 $localDir = "C:\ProgramData\"
 $localPath = "C:\ProgramData\windowsdesktop-runtime-6.0.4-win-x64.exe"
 
 # Download the Installer
 Write-Host "Downloading .NET 6.0.4, please wait..."
 Invoke-WebRequest -Uri $dotnetUrl -OutFile $localPath
+Write-Host "Done!"
 
 function Install-DotNet6 {
     Start-Process -FilePath $localPath -ArgumentList "/install /quiet /norestart" -Wait
@@ -26,5 +28,4 @@ function Install-DotNet6 {
 # Install the application silently
 Write-Host "Installing .NET 6.0.4, please wait..."
 Install-DotNet6
-
 Write-Host "Done!"
